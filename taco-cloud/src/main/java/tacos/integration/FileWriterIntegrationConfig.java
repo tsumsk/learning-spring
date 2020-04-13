80fb9eb3c5aba2556e35371230b662cc6c530cb7
liuxu <liuxuhelloworld@126.com>
2020-04-13 14:45:45 +0800

    Spring Integration, DSL configuration example

--- taco-cloud/src/main/java/tacos/integration/FileWriterIntegrationConfig.java
+++ taco-cloud/src/main/java/tacos/integration/FileWriterIntegrationConfig.java
@@ -4,17 +4,16 @@ import java.io.File;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.annotation.ServiceActivator;
-import org.springframework.integration.annotation.Transformer;
-import org.springframework.integration.channel.DirectChannel;
-import org.springframework.integration.file.FileWritingMessageHandler;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.MessageChannels;
+import org.springframework.integration.file.dsl.Files;
 import org.springframework.integration.file.support.FileExistsMode;
-import org.springframework.integration.transformer.GenericTransformer;
-import org.springframework.messaging.MessageChannel;
 
 @Configuration
 public class FileWriterIntegrationConfig {
 
+    /*
     @Bean
     public MessageChannel textInChannel() {
         return new DirectChannel();
@@ -42,5 +41,18 @@ public class FileWriterIntegrationConfig {
         handler.setAppendNewLine(true);
 
         return handler;
+    }
+     */
+
+    @Bean
+    public IntegrationFlow fileWriterFlow() {
+        return IntegrationFlows
+            .from(MessageChannels.direct("textInChannel"))
+            .<String, String>transform(t -> t.toUpperCase())
+            .handle(Files
+                .outboundAdapter(new File("/Users/baidu/tmp/spring_integration_test"))
+                .fileExistsMode(FileExistsMode.APPEND)
+                .appendNewLine(true))
+            .get();
     }
 }
