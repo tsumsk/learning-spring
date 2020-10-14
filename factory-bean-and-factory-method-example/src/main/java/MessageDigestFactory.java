02f2bbeceb877ca22a88aac33a8c9cbdd13aab0f
liuxuhelloworld <liuxuhelloworld@126.com>
2020-10-14 11:01:47 +0800

    factory-bean and factory-method example

---
+++ factory-bean-and-factory-method-example/src/main/java/MessageDigestFactory.java
@@ -0,0 +1,13 @@
+import java.security.MessageDigest;
+
+public class MessageDigestFactory {
+    private String algorithmName = "MD5";
+
+    public MessageDigest createInstance() throws Exception {
+        return MessageDigest.getInstance(algorithmName);
+    }
+
+    public void setAlgorithmName(String algorithmName) {
+        this.algorithmName = algorithmName;
+    }
+}
