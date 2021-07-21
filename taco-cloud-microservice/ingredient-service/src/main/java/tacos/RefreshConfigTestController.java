b28cb18c8d540c225ed14d919fd2790ce830537d
liuxuhelloworld <liuxuhelloworld@126.com>
2021-07-21 20:48:09 +0800

    taco cloud microservice example code

---
+++ taco-cloud-microservice/ingredient-service/src/main/java/tacos/RefreshConfigTestController.java
@@ -0,0 +1,16 @@
+package tacos;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@ConfigurationProperties(prefix = "greeting")
+@RestController
+public class RefreshConfigTestController {
+	private String message;
+
+	@GetMapping("/hello")
+	public String message() {
+		return message;
+	}
+}
